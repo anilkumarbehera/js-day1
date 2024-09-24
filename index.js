73536335b71ccbe5612900555966af0ada6db4db
Anil Kumar Behera <161294352+anilkumarbehera@users.noreply.github.com>
2024-09-24 18:02:20 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+// var fname, lname;
+// fname = "krishna";
+// lname = "radha";
+// fname = 10;
+// document.write(fname);
+
+// document.write("Hi ..");
+
+x =  10; // Assignment
+// var x;
+var x;   // Declaration
+document.write(x);
